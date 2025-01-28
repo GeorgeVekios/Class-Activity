dff5616024120ecc5c2ce649d0fcb2a44724f923
George Vekios <georgevekios@gmail.com>
2025-01-28 12:13:56 -0500

    Added button functionality - when clicked, button text changes to "Button Clicked!"

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,35 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var clickMeButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //clickMeButton = findViewById(R.id.clickMeButton)
+
+//        findViewById<Button>(R.id.clickMeButton).setOnClickListener { view: View ->
+//            (view as Button).text = "Button clicked!"
+//        }
+
+        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            (it as Button).text = "Button Clicked!"
+        }
+
+//        val ocl = object: OnClickListener{
+//            override fun onClick(p0: View?){
+//                (p0 as Button).text = "Button Clicked!"
+//            }
+//        }
     }
+
 }
